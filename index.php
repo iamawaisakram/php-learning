35edb7047fd8b6e4bc5c1b553447e76a1c67b853
Awais Mughal <awais.akram@empglabs.com>
2021-06-08 13:17:40 +0500

    Separation of concern

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+<?php
 
-  </head>
-  <body>
-    <h1> <?=$name?> </h1>
-  </body>
-</html>
+$name = htmlspecialchars($_GET['name']);
+
+require 'index.view.php';

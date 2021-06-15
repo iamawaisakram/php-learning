29f69e0c0fc77745a4725a5c1785427ccba45636
Awais Mughal <awais.akram@empglabs.com>
2021-06-15 12:50:50 +0500

    Inserting Data Dynamically to DB

--- views/index.view.php
+++ views/index.view.php
@@ -1,10 +1,18 @@
 <?php require "partials/head.php";?>
 
-  <h1> Submit your name </h1>
+  <h1> Submit your Name </h1>
 
   <form method="POST" action="/names">
     <input name="name"></input>
     <input type="submit"></input>
   </form>
+
+  <?php foreach ($users as $user): ?>
+    <ul>
+      <li>
+        <?=$user->name?>
+      </li>
+    </ul>
+  <?php endforeach;?>
 
 <?php require "partials/footer.php";?>

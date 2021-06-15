db557e36eeedb11264c3247d84d4aa7cc7122415
Awais Mughal <awais.akram@empglabs.com>
2021-06-15 13:03:15 +0500

    Composer and Autoload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require "vendor/autoload.php";
 $query = require "core/Bootstrap.php";
 
 require Router::load("routes.php")->direct(Request::uri(), Request::method());
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '29f69e0c0fc77745a4725a5c1785427ccba45636',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '29f69e0c0fc77745a4725a5c1785427ccba45636',
+            'dev_requirement' => false,
+        ),
+    ),
+);
